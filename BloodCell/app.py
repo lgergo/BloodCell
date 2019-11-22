018af119b9d95daaecdf207d81c6397ef54d148f
lgergo <lg.gergo@gmail.com>
2019-11-22 16:16:11 +0100

    Contouring

--- BloodCell/app.py
+++ BloodCell/app.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
 from PIL import Image
+#Python imaging library
 
 #flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
 #print(flags)
 
-class Test:
+class ImageProcessor:
 
     RGB_SCALE = 255;
     CMYK_SCALE = 0;
@@ -24,7 +25,7 @@ class Test:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def k_means(self, image, k):
+    def k_means(image, k):
         Z = image.reshape((-1, 3))
         # convert to np.float32
         Z = np.float32(Z)
@@ -37,41 +38,44 @@ class Test:
         res2 = res.reshape(image.shape)
         return res2
 
-    def rgb_to_grayscale(self, image):
+    def rgb_to_grayscale(image):
         img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        self.show_image(img)
+        return img
 
-    def lab(self, image):
+    def lab(image):
         im = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
-        self.show_image(im)
+        return im
 
-    def ycb(self, image):
+    def ycb(image):
         im = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-        self.show_image(im)
+        return im
 
-    def hsv_hls(self, image):
+    def hsv(image):
         hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        return hsv
+
+    def hls(image):
         hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-        self.show_images_multiple(hsv,hls, "HSV", "HLS")
+        return hls
 
-    def luv(self, image):
+    def luv(image):
         im = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
-        self.show_image(im)
+        return im
 
-    def yuv(self, image):
+    def yuv(image):
         im = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-        self.show_image(im)
+        return im
 
-    def xyz(self, image):
+    def xyz(image):
         im = cv2.cvtColor(image, cv2.COLOR_RGB2XYZ)
-        self.show_image(im)
+        return im
 
     def bgr(self, image):
         im = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         self.show_image(im)
 
     def red_channel(self, image):
-        im=image.copy()
+        im = image.copy()
         im[:, :, 0] = 0
         im[:, :, 2] = 0
         self.show_image(im)
@@ -89,25 +93,25 @@ class Test:
         B[B < M] = 0
 
         # merge the channels back together and return the image
-        im= cv2.merge([B, G, R])
+        im = cv2.merge([B, G, R])
         self.show_image(im)
 
     def red_intensity(self, image):
-        b,g,r=cv2.split(image)
+        b, g, r = cv2.split(image)
         numpy_vertical_concat = np.hstack(cv2.split(image))
         self.show_image(numpy_vertical_concat)
 
-    def color_thresholding(self, image):
-        im=cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-        mask = cv2.inRange(im, (110, 25, 25), (180, 255, 255))
+    # def color_thresholding(self, image):
+    #     im = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    #     mask = cv2.inRange(im, (110, 25, 25), (180, 255, 255))
+    #
+    #     imask = mask > 0
+    #     green = np.zeros_like(img, np.uint8)
+    #     green[imask] = img[imask]
+    #
+    #     return green
 
-        imask = mask > 0
-        green = np.zeros_like(img, np.uint8)
-        green[imask] = img[imask]
-
-        return green
-
-    def adjust_gamma(self, image, gamma=1.0):
+    def adjust_gamma(image, gamma=1.0):
         # build a lookup table mapping the pixel values [0, 255] to
         # their adjusted gamma values
         invGamma = 1.0 / gamma
@@ -115,28 +119,95 @@ class Test:
                           for i in np.arange(0, 256)]).astype("uint8")
 
         # apply gamma correction using the lookup table
-        im_lut= cv2.LUT(image, table)
-        self.show_image(im_lut)
+        im_lut = cv2.LUT(image, table)
+        return im_lut
 
-    def rgb_to_cmyk(imPath):
+    def rgb_to_cmyk_separate_channels(imPath):
         im = Image.open(imPath).convert('CMYK')
         np_image = np.array(im)
         c = np_image[:, :, 0]
         m = np_image[:, :, 1]
         y = np_image[:, :, 2]
         k = np_image[:, :, 3]
+        return (c, m ,y, k)
 
-        return (c, m ,y, k);
+    def rgb_to_cmyk(imPath):
+        im= Image.open(imPath).convert('CMYK')
+        np_image = np.array(im)
+        result=np_image[:, :, :]
+        return result
 
+    def dilate(image, kernelSize, iterations):
+        kernel = np.ones((kernelSize, kernelSize), np.uint8)
+        dilation = cv2.dilate(image, kernel, iterations)
+        return dilation
+
+    def all_channel_enhance(im, kernel):
+        for channel in im:
+            img_blur=cv2.medianBlur(channel,5)
+            img_blackhat = cv2.morphologyEx(img_blur, cv2.MORPH_BLACKHAT, kernel)
+            img_tophat = cv2.morphologyEx(img_blur, cv2.MORPH_TOPHAT, kernel)
+            channel = img_blur + img_blackhat - img_tophat
+        return im
+
+    def color_histogram_eq_separate_channels(im):
+        b, g, r = cv2.split(img)
+        red = cv2.equalizeHist(r)
+        green = cv2.equalizeHist(g)
+        blue = cv2.equalizeHist(b)
+        return cv2.merge((blue, green, red))
+
+    def contouring(imOriginal, thresholded):
+        contours, hierarchy = cv2.findContours(thresholded,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        #cnt = contours[0]
+        #M = cv2.moments(cnt)
+        #print(M)
+
+        #TODO hova kell berakni az approxot
+        # for x in contours:
+        #     epsilon = 0.1*cv2.arcLength(x,True)
+        #     approx = cv2.approxPolyDP(x,epsilon,True)
+
+        cv2.drawContours(imOriginal, contours,-1, (0,0,255), 3,cv2.LINE_4,hierarchy,1,None)
+
+        return
+
+    def find_nucleus(img, channel, k):
+        imBlur = cv2.medianBlur(img[:, :, channel], 5)
+        kmeans = ImageProcessor.k_means(imBlur, k)
+        thresh = cv2.threshold(kmeans, 127, 255, cv2.THRESH_BINARY)
+        kernel = np.zeros((11, 11), np.uint8)
+        imMorph = thresh[1].copy()
+        cv2.morphologyEx(imMorph, cv2.MORPH_CLOSE, kernel,None,None,5)
+        return  imMorph
 
 cv2.namedWindow("output", cv2.WINDOW_KEEPRATIO)
+#cv2.namedWindow("output2", cv2.WINDOW_KEEPRATIO)
 img = cv2.imread("resources/IMG_3643.jpg")
 
-cmyk=Test.rgb_to_cmyk("resources/IMG_3643.jpg");
-img=cv2.medianBlur(cmyk[1],9,img);
-kmeans=Test.k_means(Test,img,2);
-Test.show_image(kmeans);
+print(cv2.getVersionString())
 
-#Test.show_images_multiple(kmeans,kmeans2,"output","output2")
+imCmyk=ImageProcessor.rgb_to_cmyk("resources/IMG_3643.jpg")
+im1=ImageProcessor.lab(img)
+im2=ImageProcessor.xyz(img)
+im3=ImageProcessor.hsv(img) #[0]: wcb mag és rbc de nincs cyto; [1]: CCA; [2]: nincs wbc mag
+im4=ImageProcessor.hls(img) #[0]: wcb mag és rbc de nincs cyto; [1]: nincs wbc mag; [2]: CCA
+im5=ImageProcessor.ycb(img)
+im6=ImageProcessor.luv(img)
+im7=ImageProcessor.yuv(img)
+
+# WCB szempontjából jó csatornák:
+# [0] : hsv, hls, ycb, luv, yuv
+# [1] : lab, xyz, hsv, ycb, luv, cmyk
+# [2] : hsv, hls, yuv
+
+#imsum= cv2.convertScaleAbs(imCmyk[:,:,1], 1, 1.9)
+
+imcres=ImageProcessor.find_nucleus(imCmyk,1,2)
+ImageProcessor.contouring(img, imcres)
+
+ImageProcessor.show_image(img)
+
+#Test.show_images_multiple(imcres,imhsvres,"output","output2")
 
 
